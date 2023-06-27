fc3f72ef97f395d0d30863e1051de48a7028fbb0
xwzhou <xwzhou@yeah.net>
2023-06-27 11:02:24 +0800

    fix

--- sleep.go
+++ sleep.go
@@ -3,6 +3,7 @@ package xtime
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -40,42 +41,92 @@ func ReleaseTimer(t *time.Timer) *time.Timer {
 	return t
 }
 
-func SetInterval(ctx context.Context, d time.Duration, f func() error) {
-	f = ErrRecovered(f)
-	go func() {
-		t := time.NewTicker(d)
-		defer t.Stop()
-	LOOP:
-		for {
-			select {
-			case <-ctx.Done():
-				break LOOP
-			case <-t.C:
-				f()
-			}
-		}
-	}()
+func AcquireTicker(t *time.Ticker, d time.Duration) *time.Ticker {
+	if t == nil {
+		t = time.NewTicker(d)
+	} else {
+		ReleaseTicker(t)
+		t.Reset(d)
+	}
+	return t
 }
 
-func SetTimeout(ctx context.Context, d time.Duration, f func() error) {
-	f = ErrRecovered(f)
-	go func() {
-		var t *time.Timer
-		t = AcquireTimer(t, d)
-		defer ReleaseTimer(t)
+func ReleaseTicker(t *time.Ticker) *time.Ticker {
+	if t != nil {
+		t.Stop()
+		select {
+		case <-t.C:
+		default:
+		}
+	}
+	return t
+}
 
+type Future interface {
+	Stop()
+}
+
+type tickerFuture struct {
+	mutex sync.Mutex
+	t     *time.Ticker
+}
+
+func (f *tickerFuture) Stop() {
+	f.mutex.Lock()
+	t := f.t
+	f.t = nil
+	f.mutex.Unlock()
+	ReleaseTicker(t)
+}
+
+func SetInterval(ctx context.Context, d time.Duration, f func() error) Future {
+	f = ErrRecovered(f)
+	future := &tickerFuture{t: AcquireTicker(nil, d)}
+	go func() {
+		defer future.Stop()
 	LOOP:
 		for {
 			select {
 			case <-ctx.Done():
 				break LOOP
-			case <-t.C:
+			case <-future.t.C:
+				f()
+			}
+		}
+	}()
+	return future
+}
+
+type timeoutFuture struct {
+	mutex sync.Mutex
+	t     *time.Timer
+}
+
+func (f *timeoutFuture) Stop() {
+	f.mutex.Lock()
+	t := f.t
+	f.t = nil
+	f.mutex.Unlock()
+	ReleaseTimer(t)
+}
+
+func SetTimeout(ctx context.Context, d time.Duration, f func() error) Future {
+	f = ErrRecovered(f)
+	var future = &timeoutFuture{t: AcquireTimer(nil, d)}
+	go func() {
+		defer future.Stop()
+	LOOP:
+		for {
+			select {
+			case <-ctx.Done():
+				break LOOP
+			case <-future.t.C:
 				f()
 				break LOOP
 			}
 		}
-
 	}()
+	return future
 }
 
 func WithTimeout(ctx context.Context, d time.Duration, f func(ctx context.Context) error) func() error {
